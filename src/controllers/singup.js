2289d137453aa587dd30650d3af1eac6f3984186
MiralPatel-017 <miralkamleshbhai@gmail.com>
2025-03-24 16:44:47 +0530

    create signup controller and route

---
+++ src/controllers/singup.js
@@ -0,0 +1,16 @@
+const { model } = require('mongoose');
+const userServices = require('../services/signup');
+
+async function createUser(req, res) {
+    try {
+        const { name, email, password } = req.body;
+        const user = await userServices.createUser(name, email, password);
+        res.status(200).json({ user: user , message: 'message from ./src/controllers/signup: User created successfully' });
+
+    } catch (error) {
+        console.error("error from ./src/controllers/signup: " + error);
+        res.status(400).json({ message: "Error creating user : " + error.message });
+    }
+}
+
+module.exports = { createUser };
